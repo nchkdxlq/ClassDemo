01bef19ce90f65e39831a29f72e8263ee5783215
luoquan <nchkdxlq.gmail.com>
2018-05-13 17:52:07 +0800

    Objective-C class

---
+++ ClassDemo/ClassDemo/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  ClassDemo
+//
+//  Created by nchkdxlq on 2018/5/13.
+//  Copyright © 2018年 luoquan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person: NSObject {
+    int _age;
+    double _height;
+}
+
+@end
+
+@implementation Person
+@end
+
+//////////////////////////
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ClassDemo
+# Objective-C 类结构分析
+
 分析 Instance、class、meta-class 之间的关系
+[参考阅读](https://www.jianshu.com/p/39ca35c31a1b)
